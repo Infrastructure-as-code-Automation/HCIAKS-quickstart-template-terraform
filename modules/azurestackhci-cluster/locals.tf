e280eb1996d42e85d2729c3306b79b81d9c4a57d
qfai <quke@microsoft.com>
2024-09-02 13:31:19 +0800

    remove all modules except base

--- modules/azurestackhci-cluster/locals.tf
+++
@@ -1,8 +0,0 @@
-locals {
-  decoded_user_storages              = jsondecode(data.azapi_resource_list.user_storages.output).value
-  owned_user_storages                = [for storage in local.decoded_user_storages : storage if lower(storage.extendedLocation.name) == lower(data.azapi_resource.customlocation.id)]
-  role_definition_resource_substring = "/providers/Microsoft.Authorization/roleDefinitions"
-  rp_roles = {
-    ACMRM = "Azure Connected Machine Resource Manager",
-  }
-}
--- modules/hci-server-provisioner/locals.tf
+++
@@ -1,8 +0,0 @@
-locals {
-  roles = {
-    KVSU   = "Key Vault Secrets User",
-    ACMRM  = "Azure Connected Machine Resource Manager",
-    ASHDMR = "Azure Stack HCI Device Management Role",
-    Reader = "Reader"
-  }
-}
